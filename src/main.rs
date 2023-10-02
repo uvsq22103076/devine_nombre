459f5b3e2c45540d62f604c837c38d852e10e5d7
Hugo Tieres <hugo.tieres@gmail.com>
2023-10-02 14:33:10 +0200

    1.0.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use console::Style; // Importez la bibliothèque console.
+
 
 fn main() {
-
-    println!("Devine le nombre!");
+    // Affiche un message d'accueil en bleu.
+    let style = Style::new().blue();
+    println!("{}", style.apply_to("Devine le nombre!"));
 
     let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-    println!("Le nombre secret est : {secret_number}");
+    // Affiche le nombre secret uniquement en mode de débogage.
+    #[cfg(debug_assertions)]
+    {
+        println!("Le nombre secret est : {secret_number}");
+    }
 
     loop {
-        println!("Choisit un nombre entre 1 et 100.");
+        println!("Choisissez un nombre entre 1 et 100.");
 
         let mut choix = String::new();
 
@@ -19,16 +26,24 @@ fn main() {
             .read_line(&mut choix)
             .expect("Erreur de lecture");
 
-        let nombre: u32 = choix.trim().parse().expect("Veuillez entrer un nombre!");
+        // Tente de convertir l'entrée utilisateur en un nombre u32.
+        let nombre: Result<u32, _> = choix.trim().parse();
         
-            println!("You guessed: {choix}");
+        match nombre {
+            Ok(nombre) => {
+                println!("You guessed: {}", nombre);
 
-        match nombre.cmp(&secret_number) {
-            Ordering::Less => println!("Trop petit!"),
-            Ordering::Greater => println!("Trop grand!"),
-            Ordering::Equal => {
-                println!("Tu as trouvé!");
-                break;
+                match nombre.cmp(&secret_number) {
+                    Ordering::Less => println!("Trop petit!"),
+                    Ordering::Greater => println!("Trop grand!"),
+                    Ordering::Equal => {
+                        println!("Tu as trouvé!");
+                        break;
+                    }
+                }
+            }
+            Err(_) => {
+                println!("Veuillez entrer un nombre valide!");
             }
         }
     }
